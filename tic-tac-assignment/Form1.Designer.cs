73dbd2df12e7489a1a32e2fb5e6ace206a135ac2
Serzen <sericomput@gmail.com>
2023-04-27 16:22:43 +0300

    - Buttons change to X or O on click depending on player - label1 changes to match current player turn - game checks for winner in horizontal, vertical and diagonal

--- tic-tac-assignment/Form1.Designer.cs
+++ tic-tac-assignment/Form1.Designer.cs
@@ -29,17 +29,17 @@
         private void InitializeComponent()
         {
             panel1 = new Panel();
-            panel2 = new Panel();
             label1 = new Label();
-            button1 = new Button();
-            button2 = new Button();
-            button3 = new Button();
-            button4 = new Button();
-            button5 = new Button();
-            button6 = new Button();
-            button7 = new Button();
-            button8 = new Button();
+            panel2 = new Panel();
             button9 = new Button();
+            button8 = new Button();
+            button7 = new Button();
+            button6 = new Button();
+            button5 = new Button();
+            button4 = new Button();
+            button3 = new Button();
+            button2 = new Button();
+            button1 = new Button();
             panel1.SuspendLayout();
             panel2.SuspendLayout();
             SuspendLayout();
@@ -48,10 +48,21 @@
             // 
             panel1.BackColor = Color.Crimson;
             panel1.Controls.Add(label1);
-            panel1.Location = new Point(43, 27);
+            panel1.Location = new Point(12, 27);
             panel1.Name = "panel1";
-            panel1.Size = new Size(522, 92);
+            panel1.Size = new Size(585, 92);
             panel1.TabIndex = 0;
+            panel1.Paint += panel1_Paint;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("UD Digi Kyokasho NK-B", 32F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(16, 15);
+            label1.Name = "label1";
+            label1.Size = new Size(203, 63);
+            label1.TabIndex = 0;
+            label1.Text = "label1";
             // 
             // panel2
             // 
@@ -70,97 +81,95 @@
             panel2.Size = new Size(585, 580);
             panel2.TabIndex = 1;
             // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.Font = new Font("UD Digi Kyokasho NK-B", 36F, FontStyle.Bold, GraphicsUnit.Point);
-            label1.Location = new Point(14, 11);
-            label1.Name = "label1";
-            label1.Size = new Size(225, 70);
-            label1.TabIndex = 0;
-            label1.Text = "label1";
-            // 
-            // button1
-            // 
-            button1.Location = new Point(31, 25);
-            button1.Name = "button1";
-            button1.Size = new Size(165, 165);
-            button1.TabIndex = 0;
-            button1.Text = "button1";
-            button1.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            button2.Location = new Point(208, 25);
-            button2.Name = "button2";
-            button2.Size = new Size(166, 165);
-            button2.TabIndex = 1;
-            button2.Text = "button2";
-            button2.UseVisualStyleBackColor = true;
-            button2.Click += button2_Click;
-            // 
-            // button3
-            // 
-            button3.Location = new Point(388, 25);
-            button3.Name = "button3";
-            button3.Size = new Size(165, 165);
-            button3.TabIndex = 2;
-            button3.Text = "button3";
-            button3.UseVisualStyleBackColor = true;
-            // 
-            // button4
-            // 
-            button4.Location = new Point(31, 205);
-            button4.Name = "button4";
-            button4.Size = new Size(165, 165);
-            button4.TabIndex = 3;
-            button4.Text = "button4";
-            button4.UseVisualStyleBackColor = true;
-            // 
-            // button5
-            // 
-            button5.Location = new Point(209, 205);
-            button5.Name = "button5";
-            button5.Size = new Size(165, 165);
-            button5.TabIndex = 4;
-            button5.Text = "button5";
-            button5.UseVisualStyleBackColor = true;
-            // 
-            // button6
-            // 
-            button6.Location = new Point(388, 205);
-            button6.Name = "button6";
-            button6.Size = new Size(165, 165);
-            button6.TabIndex = 5;
-            button6.Text = "button6";
-            button6.UseVisualStyleBackColor = true;
-            // 
-            // button7
-            // 
-            button7.Location = new Point(31, 387);
-            button7.Name = "button7";
-            button7.Size = new Size(165, 165);
-            button7.TabIndex = 6;
-            button7.Text = "button7";
-            button7.UseVisualStyleBackColor = true;
-            // 
-            // button8
-            // 
-            button8.Location = new Point(209, 387);
-            button8.Name = "button8";
-            button8.Size = new Size(165, 165);
-            button8.TabIndex = 7;
-            button8.Text = "button8";
-            button8.UseVisualStyleBackColor = true;
-            // 
             // button9
             // 
-            button9.Location = new Point(388, 387);
+            button9.Font = new Font("UD Digi Kyokasho NK-B", 36F, FontStyle.Bold, GraphicsUnit.Point);
+            button9.Location = new Point(389, 387);
             button9.Name = "button9";
             button9.Size = new Size(165, 165);
             button9.TabIndex = 8;
-            button9.Text = "button9";
             button9.UseVisualStyleBackColor = true;
+            button9.Click += btn_click;
+            // 
+            // button8
+            // 
+            button8.Font = new Font("UD Digi Kyokasho NK-B", 36F, FontStyle.Bold, GraphicsUnit.Point);
+            button8.Location = new Point(210, 387);
+            button8.Name = "button8";
+            button8.Size = new Size(165, 165);
+            button8.TabIndex = 7;
+            button8.UseVisualStyleBackColor = true;
+            button8.Click += btn_click;
+            // 
+            // button7
+            // 
+            button7.Font = new Font("UD Digi Kyokasho NK-B", 36F, FontStyle.Bold, GraphicsUnit.Point);
+            button7.Location = new Point(32, 387);
+            button7.Name = "button7";
+            button7.Size = new Size(165, 165);
+            button7.TabIndex = 6;
+            button7.UseVisualStyleBackColor = true;
+            button7.Click += btn_click;
+            // 
+            // button6
+            // 
+            button6.Font = new Font("UD Digi Kyokasho NK-B", 36F, FontStyle.Bold, GraphicsUnit.Point);
+            button6.Location = new Point(389, 205);
+            button6.Name = "button6";
+            button6.Size = new Size(165, 165);
+            button6.TabIndex = 5;
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += btn_click;
+            // 
+            // button5
+            // 
+            button5.Font = new Font("UD Digi Kyokasho NK-B", 36F, FontStyle.Bold, GraphicsUnit.Point);
+            button5.Location = new Point(210, 205);
+            button5.Name = "button5";
+            button5.Size = new Size(165, 165);
+            button5.TabIndex = 4;
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += btn_click;
+            // 
+            // button4
+            // 
+            button4.Font = new Font("UD Digi Kyokasho NK-B", 36F, FontStyle.Bold, GraphicsUnit.Point);
+            button4.Location = new Point(32, 205);
+            button4.Name = "button4";
+            button4.Size = new Size(165, 165);
+            button4.TabIndex = 3;
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += btn_click;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("UD Digi Kyokasho NK-B", 36F, FontStyle.Bold, GraphicsUnit.Point);
+            button3.Location = new Point(389, 25);
+            button3.Name = "button3";
+            button3.Size = new Size(165, 165);
+            button3.TabIndex = 2;
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += btn_click;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("UD Digi Kyokasho NK-B", 36F, FontStyle.Bold, GraphicsUnit.Point);
+            button2.Location = new Point(209, 25);
+            button2.Name = "button2";
+            button2.Size = new Size(166, 165);
+            button2.TabIndex = 1;
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += btn_click;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("UD Digi Kyokasho NK-B", 36F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.Location = new Point(32, 25);
+            button1.Name = "button1";
+            button1.Size = new Size(165, 165);
+            button1.TabIndex = 0;
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += btn_click;
             // 
             // Form1
             // 
--- tic-tac-assignment/Form1.cs
+++ tic-tac-assignment/Form1.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+
 namespace tic_tac_assignment
 {
     public partial class Form1 : Form
@@ -7,7 +9,101 @@ namespace tic_tac_assignment
             InitializeComponent();
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            foreach (Control c in panel2.Controls)
+            {
+                if (c is Button)
+                {
+                    c.Click += new System.EventHandler(btn_click);
+
+                }
+            }
+        }
+
+
+        int XorO = 0;
+        char curPlayer = 'X';
+        bool win = false;
+        //button action
+        public void btn_click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            Debug.WriteLine(btn.Name);
+            if (btn.Text.Equals(""))
+            {
+
+                if (!win)
+                {
+                    if (XorO % 2 == 0)
+                    {
+                        btn.Text = "X";
+                        btn.ForeColor = Color.Red;
+                        //getWinner();
+                        curPlayer = 'X';
+
+                        label1.Text = "O's Turn Now";
+                    }
+                    else
+                    {
+                        btn.Text = "O";
+                        btn.ForeColor = Color.Blue;
+                        curPlayer = 'O';
+
+                        //getWinner();
+
+                        label1.Text = "X's Turn Now";
+
+                    }
+                    XorO++;
+
+                }
+                getWinner();
+
+            }
+        }
+
+        public void getWinner()
+        {
+            string winText = curPlayer + " Is The Winner";
+            //check horizontal
+            if ((button1.Text != "" && button1.Text == button2.Text && button2.Text == button3.Text)
+                || (button4.Text != "" && button4.Text == button5.Text && button5.Text == button6.Text)
+                || (button7.Text != "" && button7.Text == button8.Text && button8.Text == button9.Text)
+                )
+            {
+                win = true;
+                label1.Text = winText;
+
+            } //check vertical
+            else if ((button1.Text != "" && button1.Text == button4.Text && button4.Text == button7.Text)
+                || (button2.Text != "" && button2.Text == button5.Text && button5.Text == button8.Text)
+                || (button3.Text != "" && button3.Text == button6.Text && button6.Text == button9.Text)
+                )
+            {
+                win = true;
+                label1.Text = winText;
+
+
+            }//check diagonal
+            else if ((button1.Text != "" && button1.Text == button5.Text && button5.Text == button9.Text)
+                || (button3.Text != "" && button3.Text == button5.Text && button5.Text == button7.Text)
+                )
+            {
+                win = true;
+                label1.Text = winText;
+
+            }
+            else if (button1.Text != "" && button2.Text != "" && button3.Text != ""
+                  && button4.Text != "" && button5.Text != "" && button6.Text != ""
+                  && button7.Text != "" && button8.Text != "" && button9.Text != "")
+            {
+                label1.Text = "Game Ended In Draw";
+
+            }
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
         {
 
         }
