04af72d400b184693a4f8d5f505c92df74ab01b2
Serzen <sericomput@gmail.com>
2023-04-27 19:16:33 +0300

    -label1 initial text changed to "Play Now", color changed to -Button default, hover, and down colors changed - getWinner changed to check rows,cols and diagonals individually -game now highlights winning squares -playing field buttons are disabled upon game end.

--- tic-tac-assignment/Form1.Designer.cs
+++ tic-tac-assignment/Form1.Designer.cs
@@ -57,12 +57,14 @@
             // label1
             // 
             label1.AutoSize = true;
+            label1.BackColor = Color.Transparent;
             label1.Font = new Font("UD Digi Kyokasho NK-B", 32F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.ForeColor = Color.AliceBlue;
             label1.Location = new Point(16, 15);
             label1.Name = "label1";
-            label1.Size = new Size(203, 63);
+            label1.Size = new Size(284, 63);
             label1.TabIndex = 0;
-            label1.Text = "label1";
+            label1.Text = "Play Now";
             // 
             // panel2
             // 
@@ -83,92 +85,137 @@
             // 
             // button9
             // 
+            button9.BackColor = Color.AliceBlue;
+            button9.FlatAppearance.MouseDownBackColor = Color.LightSteelBlue;
+            button9.FlatAppearance.MouseOverBackColor = Color.Lavender;
+            button9.FlatStyle = FlatStyle.Flat;
             button9.Font = new Font("UD Digi Kyokasho NK-B", 36F, FontStyle.Bold, GraphicsUnit.Point);
+            button9.ForeColor = Color.Black;
             button9.Location = new Point(389, 387);
             button9.Name = "button9";
             button9.Size = new Size(165, 165);
             button9.TabIndex = 8;
-            button9.UseVisualStyleBackColor = true;
+            button9.UseVisualStyleBackColor = false;
             button9.Click += btn_click;
             // 
             // button8
             // 
+            button8.BackColor = Color.AliceBlue;
+            button8.FlatAppearance.MouseDownBackColor = Color.LightSteelBlue;
+            button8.FlatAppearance.MouseOverBackColor = Color.Lavender;
+            button8.FlatStyle = FlatStyle.Flat;
             button8.Font = new Font("UD Digi Kyokasho NK-B", 36F, FontStyle.Bold, GraphicsUnit.Point);
+            button8.ForeColor = Color.Black;
             button8.Location = new Point(210, 387);
             button8.Name = "button8";
             button8.Size = new Size(165, 165);
             button8.TabIndex = 7;
-            button8.UseVisualStyleBackColor = true;
+            button8.UseVisualStyleBackColor = false;
             button8.Click += btn_click;
             // 
             // button7
             // 
+            button7.BackColor = Color.AliceBlue;
+            button7.FlatAppearance.MouseDownBackColor = Color.LightSteelBlue;
+            button7.FlatAppearance.MouseOverBackColor = Color.Lavender;
+            button7.FlatStyle = FlatStyle.Flat;
             button7.Font = new Font("UD Digi Kyokasho NK-B", 36F, FontStyle.Bold, GraphicsUnit.Point);
+            button7.ForeColor = Color.Black;
             button7.Location = new Point(32, 387);
             button7.Name = "button7";
             button7.Size = new Size(165, 165);
             button7.TabIndex = 6;
-            button7.UseVisualStyleBackColor = true;
+            button7.UseVisualStyleBackColor = false;
             button7.Click += btn_click;
             // 
             // button6
             // 
+            button6.BackColor = Color.AliceBlue;
+            button6.FlatAppearance.MouseDownBackColor = Color.LightSteelBlue;
+            button6.FlatAppearance.MouseOverBackColor = Color.Lavender;
+            button6.FlatStyle = FlatStyle.Flat;
             button6.Font = new Font("UD Digi Kyokasho NK-B", 36F, FontStyle.Bold, GraphicsUnit.Point);
+            button6.ForeColor = Color.Black;
             button6.Location = new Point(389, 205);
             button6.Name = "button6";
             button6.Size = new Size(165, 165);
             button6.TabIndex = 5;
-            button6.UseVisualStyleBackColor = true;
+            button6.UseVisualStyleBackColor = false;
             button6.Click += btn_click;
             // 
             // button5
             // 
+            button5.BackColor = Color.AliceBlue;
+            button5.FlatAppearance.MouseDownBackColor = Color.LightSteelBlue;
+            button5.FlatAppearance.MouseOverBackColor = Color.Lavender;
+            button5.FlatStyle = FlatStyle.Flat;
             button5.Font = new Font("UD Digi Kyokasho NK-B", 36F, FontStyle.Bold, GraphicsUnit.Point);
+            button5.ForeColor = Color.Black;
             button5.Location = new Point(210, 205);
             button5.Name = "button5";
             button5.Size = new Size(165, 165);
             button5.TabIndex = 4;
-            button5.UseVisualStyleBackColor = true;
+            button5.UseVisualStyleBackColor = false;
             button5.Click += btn_click;
             // 
             // button4
             // 
+            button4.BackColor = Color.AliceBlue;
+            button4.FlatAppearance.MouseDownBackColor = Color.LightSteelBlue;
+            button4.FlatAppearance.MouseOverBackColor = Color.Lavender;
+            button4.FlatStyle = FlatStyle.Flat;
             button4.Font = new Font("UD Digi Kyokasho NK-B", 36F, FontStyle.Bold, GraphicsUnit.Point);
+            button4.ForeColor = Color.Black;
             button4.Location = new Point(32, 205);
             button4.Name = "button4";
             button4.Size = new Size(165, 165);
             button4.TabIndex = 3;
-            button4.UseVisualStyleBackColor = true;
+            button4.UseVisualStyleBackColor = false;
             button4.Click += btn_click;
             // 
             // button3
             // 
+            button3.BackColor = Color.AliceBlue;
+            button3.FlatAppearance.MouseDownBackColor = Color.LightSteelBlue;
+            button3.FlatAppearance.MouseOverBackColor = Color.Lavender;
+            button3.FlatStyle = FlatStyle.Flat;
             button3.Font = new Font("UD Digi Kyokasho NK-B", 36F, FontStyle.Bold, GraphicsUnit.Point);
+            button3.ForeColor = Color.Black;
             button3.Location = new Point(389, 25);
             button3.Name = "button3";
             button3.Size = new Size(165, 165);
             button3.TabIndex = 2;
-            button3.UseVisualStyleBackColor = true;
+            button3.UseVisualStyleBackColor = false;
             button3.Click += btn_click;
             // 
             // button2
             // 
+            button2.BackColor = Color.AliceBlue;
+            button2.FlatAppearance.MouseDownBackColor = Color.LightSteelBlue;
+            button2.FlatAppearance.MouseOverBackColor = Color.Lavender;
+            button2.FlatStyle = FlatStyle.Flat;
             button2.Font = new Font("UD Digi Kyokasho NK-B", 36F, FontStyle.Bold, GraphicsUnit.Point);
+            button2.ForeColor = Color.Black;
             button2.Location = new Point(209, 25);
             button2.Name = "button2";
             button2.Size = new Size(166, 165);
             button2.TabIndex = 1;
-            button2.UseVisualStyleBackColor = true;
+            button2.UseVisualStyleBackColor = false;
             button2.Click += btn_click;
             // 
             // button1
             // 
+            button1.BackColor = Color.AliceBlue;
+            button1.FlatAppearance.MouseDownBackColor = Color.LightSteelBlue;
+            button1.FlatAppearance.MouseOverBackColor = Color.Lavender;
+            button1.FlatStyle = FlatStyle.Flat;
             button1.Font = new Font("UD Digi Kyokasho NK-B", 36F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.ForeColor = Color.Black;
             button1.Location = new Point(32, 25);
             button1.Name = "button1";
             button1.Size = new Size(165, 165);
             button1.TabIndex = 0;
-            button1.UseVisualStyleBackColor = true;
+            button1.UseVisualStyleBackColor = false;
             button1.Click += btn_click;
             // 
             // Form1
--- tic-tac-assignment/Form1.cs
+++ tic-tac-assignment/Form1.cs
@@ -9,19 +9,6 @@ namespace tic_tac_assignment
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            foreach (Control c in panel2.Controls)
-            {
-                if (c is Button)
-                {
-                    c.Click += new System.EventHandler(btn_click);
-
-                }
-            }
-        }
-
-
         int XorO = 0;
         char curPlayer = 'X';
         bool win = false;
@@ -39,9 +26,7 @@ namespace tic_tac_assignment
                     {
                         btn.Text = "X";
                         btn.ForeColor = Color.Red;
-                        //getWinner();
                         curPlayer = 'X';
-
                         label1.Text = "O's Turn Now";
                     }
                     else
@@ -49,9 +34,6 @@ namespace tic_tac_assignment
                         btn.Text = "O";
                         btn.ForeColor = Color.Blue;
                         curPlayer = 'O';
-
-                        //getWinner();
-
                         label1.Text = "X's Turn Now";
 
                     }
@@ -66,43 +48,102 @@ namespace tic_tac_assignment
         public void getWinner()
         {
             string winText = curPlayer + " Is The Winner";
-            //check horizontal
-            if ((button1.Text != "" && button1.Text == button2.Text && button2.Text == button3.Text)
-                || (button4.Text != "" && button4.Text == button5.Text && button5.Text == button6.Text)
-                || (button7.Text != "" && button7.Text == button8.Text && button8.Text == button9.Text)
-                )
+            //check horizontal (top to bottom)
+            //first row
+            if (button1.Text != "" && button1.Text == button2.Text && button2.Text == button3.Text)
             {
                 win = true;
                 label1.Text = winText;
-
-            } //check vertical
-            else if ((button1.Text != "" && button1.Text == button4.Text && button4.Text == button7.Text)
-                || (button2.Text != "" && button2.Text == button5.Text && button5.Text == button8.Text)
-                || (button3.Text != "" && button3.Text == button6.Text && button6.Text == button9.Text)
-                )
-            {
-                win = true;
-                label1.Text = winText;
-
-
-            }//check diagonal
-            else if ((button1.Text != "" && button1.Text == button5.Text && button5.Text == button9.Text)
-                || (button3.Text != "" && button3.Text == button5.Text && button5.Text == button7.Text)
-                )
-            {
-                win = true;
-                label1.Text = winText;
-
+                winEffect(button1, button2, button3);
             }
+            //second row
+            else if (button4.Text != "" && button4.Text == button5.Text && button5.Text == button6.Text)
+            {
+                win = true;
+                label1.Text = winText;
+                winEffect(button4, button5, button6);
+            }
+            //third row
+            else if (button7.Text != "" && button7.Text == button8.Text && button8.Text == button9.Text)
+            {
+                win = true;
+                label1.Text = winText;
+                winEffect(button7, button8, button9);
+            }
+
+            //check vertical (left to right)
+            //first column
+            if (button1.Text != "" && button1.Text == button4.Text && button4.Text == button7.Text)
+            {
+                win = true;
+                label1.Text = winText;
+                winEffect(button1, button4, button7);
+            }
+            //second column
+            else if (button2.Text != "" && button2.Text == button5.Text && button5.Text == button8.Text)
+            {
+                win = true;
+                label1.Text = winText;
+                winEffect(button2, button5, button8);
+            }
+            //third column
+            else if (button3.Text != "" && button3.Text == button6.Text && button6.Text == button9.Text)
+            {
+                win = true;
+                label1.Text = winText;
+                winEffect(button3, button6, button9);
+            }
+
+            //check diagonal
+            // \ of X
+            if (button1.Text != "" && button1.Text == button5.Text && button5.Text == button9.Text)
+            {
+                win = true;
+                label1.Text = winText;
+                winEffect(button1, button5, button9);
+            }
+            // / of X
+             else if (button3.Text != "" && button3.Text == button5.Text && button5.Text == button7.Text)
+            {
+                win = true;
+                label1.Text = winText;
+                winEffect(button3, button5, button7);
+            }
+
+
+            if (win) //disable buttons if victor
+            {
+                disableButtons();
+            }
+            //check for draw
             else if (button1.Text != "" && button2.Text != "" && button3.Text != ""
                   && button4.Text != "" && button5.Text != "" && button6.Text != ""
                   && button7.Text != "" && button8.Text != "" && button9.Text != "")
             {
-                label1.Text = "Game Ended In Draw";
-
+                label1.Text = "Draw";
+                disableButtons(); // disable buttons if no victor
             }
         }
 
+        private void disableButtons()
+        {
+            foreach (Control c in panel2.Controls)
+            {
+                Button b = (Button)c;
+                b.Enabled = false;
+            }
+        }
+        public void winEffect(Button b1, Button b2, Button b3)
+        {
+            b1.BackColor = Color.Green;
+            b2.BackColor = Color.Green;
+            b3.BackColor = Color.Green;
+
+            b1.ForeColor = Color.White;
+            b2.ForeColor = Color.White;
+            b3.ForeColor = Color.White;
+
+        }
         private void panel1_Paint(object sender, PaintEventArgs e)
         {
 
